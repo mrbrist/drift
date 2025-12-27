774abe27661984ab921551b61779306b04d5fdb9
Joshua Weston <bristol2801@gmail.com>
2025-12-27 21:56:06 +0000

    Added sign in with google button

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,31 @@
-// import { useState } from 'react'
+import { useEffect } from 'react'
 import driftLogo from './assets/drift-logo.svg'
 import './App.css'
 
+declare global {
+  interface Window {
+    google: any
+  }
+}
+
 function App() {
+  useEffect(() => {
+    window.google.accounts.id.initialize({
+      client_id: import.meta.env.VITE_GOOGLE_CLIENT_ID,
+      callback: (response: any) => {
+        console.log('JWT:', response.credential)
+      },
+    })
+
+    window.google.accounts.id.renderButton(
+      document.getElementById('googleSignIn')!,
+      {
+        theme: 'outline',
+        size: 'large',
+        width: 250,
+      }
+    )
+  }, [])
   return (
     <>
     <div className="items-center justify-center grid grid-cols-1">
@@ -11,9 +34,12 @@ function App() {
       </div>
       <div>
         <h1 className="text-4xl font-bold text-white inline-block">drift</h1>
-        <h2 className='mt-4'>Simple kanban boards for focused project management</h2>
+        <h2 className='mt-4 text-white'>Simple kanban boards for focused project management</h2>
       </div>
-      <div className='mt-20'>Login with Google</div>
+      <div className='mt-20'>
+        {/* Login with Google */}
+        <div id="googleSignIn" className='justify-center flex'></div>
+      </div>
     </div>
     </>
   )
